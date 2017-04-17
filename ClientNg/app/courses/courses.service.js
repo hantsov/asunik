b5fd7e8cdbf91e458d730a6043149546402adfbc
Hardi Antsov <hardi.antsov@itcollege.ee>
2017-04-17 23:27:14 +0300

    Courses functionality

---
+++ ClientNg/app/courses/courses.service.js
@@ -0,0 +1,38 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .service('coursesService', coursesService);
+
+
+    function coursesService($http) {
+        var shared = this;
+
+        var serviceAddress = "http://localhost:57953/api/courses";
+
+        shared.getCourses = function () {
+            return $http.get(serviceAddress).then(function (response) {
+                return response.data;
+            });
+        };
+
+        shared.getCourse = function (courseId) {
+            return $http.get(serviceAddress + "/" + courseId).then(function (response) {
+                return response.data;
+            });
+        };
+
+        shared.createCourse = function (course) {
+            return $http.post(serviceAddress, course).then(function (response) {
+                return response.data;
+            });
+        }
+
+        shared.updateCourse = function (course) {
+            return $http.put(serviceAddress + "/" + course.id, course).then(function (response) {
+                return response.data;
+            });
+        }
+    }
+})();
